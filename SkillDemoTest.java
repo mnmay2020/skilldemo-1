ca2af83bd47d9336cd2bc2c7967b0f8fe6795523
mnmay2020 <103155657+mnmay2020@users.noreply.github.com>
2022-05-01 16:17:49 -0700

    fixed test

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemoTest {
+    
+    @Test
+    public void multiplyTester() {
+        assertEquals(6, SkillDemo1.multiply(2,3));
+    }
+}
